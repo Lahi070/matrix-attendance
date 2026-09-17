49d10eff642ba3ea22edbef82ccd07706f867223
Lahiru Srinath <161489818+Lahi070@users.noreply.github.com>
2026-09-17 16:24:15 +0530

    test db dump

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -5,7 +5,7 @@ import { supabase } from '@/lib/supabase';
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
-  const { data, error } = await supabase.from('modules').select('*');
-  return NextResponse.json(data);
+  const { data: modules } = await supabase.from('modules').select('*');
+  const { data: team_members } = await supabase.from('team_members').select('name, module_id');
+  return NextResponse.json({ modules, count: team_members?.length });
 }
-
